cee2ef0977db35fdfd7d6f377538a9368dfc6932
alvynah <alvynahw@gmail.com>
2021-02-24 21:32:51 +0300

    Write the jquery functions and javascript, to enable the leap year calculation

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("form#myForm").submit(function(event) {
+        event.preventDefault();
+        var year = parseInt($("input#year").val());
+        var result = leapYear(year);
+        $("#result").text(result);
+    });
+});
+
+var leapYear = function(year) {
+    return false;
+};
+
+var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+};
